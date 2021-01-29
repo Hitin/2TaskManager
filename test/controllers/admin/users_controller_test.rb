ec266894a4f456558a996d1a24019d30be51236d
Petr Ablyazov <phoenix-h@mail.ru>
2021-01-29 11:34:46 +0400

    Create and Update Users

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -6,14 +6,36 @@ class Admin::UsersControllerTest < ActionController::TestCase
     sign_in admin
   end
 
+  test 'should get index' do
+    get :index
+    assert_response :success
+  end
+
   test 'should get show' do
     user = create(:user)
     get :show, params: { id: user.id }
     assert_response :success
   end
 
-  test 'should get index' do
-    get :index
+  test 'should get edit' do
+    user = create(:user)
+    get :edit, params: { id: user.id }
     assert_response :success
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test 'should post create' do
+    post :create, params: { user: attributes_for(:user) }
+    assert_response :redirect
+  end
+
+  test 'should patch update' do
+    user = create(:user)
+    patch :update, params: { id: user.id, user: attributes_for(:user) }
+    assert_response :redirect
   end
 end
