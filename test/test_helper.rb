cb57c7529e81d5a6c41947e153b5bae9e2498262
Petr Ablyazov <phoenix-h@mail.ru>
2021-01-29 11:05:25 +0400

    Authorization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
-  # Run tests in parallel with specified workers
+  include AuthHelper
   parallelize(workers: :number_of_processors)
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
